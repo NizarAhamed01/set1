45d79c498bbf714658b88c6cafd65fbfb36eeaea
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-11 19:18:50 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+	int main()
+	{
+	  char c;
+	  printf("Enter a character\n");
+	  scanf("%c",&c);
+	  if((c >= 'A' && c <= 'Z' )|| (c >='a' && c <= 'z'))
+	  {
+	    if(c == 'a' || c == 'e' || c == 'i' || c== 'o' || c == 'u')
+	    {
+	      printf("given character is vowel");
+	    }
+	    else
+	    {
+	      printf("Given character is consonant");
+	    }
+	  }
+	  else
+	  {
+	    printf("Invalid Input");
+	  }
+	 return 0; 
+	}
