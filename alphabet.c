7bf134b58546375c3a16dcd29981dbb3adc362c4
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-11 19:24:56 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+	int main()
+	{
+	  int input;
+	  printf("Enter the input\n");
+	  scanf("%c",&input);
+	  if((input >='A' && input >='z') || (input >= 'a' && input <= 'z'))
+	  {
+	    printf("Alphabet");
+	  }
+	  else
+	  {
+	    printf("No");
+	   }
+	  return 0;
+	}
+
