e2f3f68b4e67b21c994953b879aa9f80183a24c0
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-11 12:15:48 +0530

    OddorEven.java

---
+++ OddorEven.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class OddorEven
+{
+public static void main(String args[])
+{
+int n;
+Scanner s=new Scanner(System.in);
+if(n%2==0)
+{
+System.out.println("Number is Odd");
+}
+elseif(n%2!=0)
+{
+System.out.println("Number is Even");
+}
+else
+{
+System.out.println("Invalid Number");
+}
+}
+}
