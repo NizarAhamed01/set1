9610ade6ed73a379f6289aa607ca4131c283d025
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-19 20:09:41 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,n,temp;
+  scanf("%d",&n);
+  int arr[n];
+  if(n <= 100000)
+  {
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+      for(j=i+1;j<n;j++)
+      {
+        if(arr[i] > arr[j])
+        {
+          temp=arr[i];
+          arr[i]=arr[j];
+          arr[j]=temp;
+        }
+      }
+    }
+    for(i=0;i<n;i++)
+    {
+      printf("%d\t",arr[i]);
+    }
+  }
+  return 0;
+}
