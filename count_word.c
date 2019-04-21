68661dbeedb29783e4c86eba97b071d69fd2bbca
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-21 13:22:49 +0530

    Create count_word.c

---
+++ count_word.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  char input[50],word=1,i;
+  gets(input);
+  for(i=0;input[i] !='\0';i++)
+  {
+    if(input[i]== ' ')
+    {
+      word++;
+    }
+  }
+  printf("%d",word);
+  return 0;
+}
