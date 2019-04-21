e812d34495fa7819aafdcfbdbe83a0902680f7d4
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-21 18:24:49 +0530

    Update nearest_even.c

--- nearest_even.c
+++ nearest_even.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-  int n;
-  scanf("%d",&n);
-  if(n % 2 == 0)
+  int num;
+  scanf("%d",&num);
+  if(num % 2 == 0)
   {
-    printf("%d",n);
+    printf("%d",num);
   }
   else
   {
-    n=n-1;
-    printf("%d",n);
+    num=num-1;
+    printf("%d",num);
   }
   return 0;
 }
