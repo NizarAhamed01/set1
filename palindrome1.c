8f31b5b6416648158aa86e71d81c3c54e9671a8e
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-25 21:12:43 +0530

    Update palindrome1.c

--- palindrome1.c
+++ palindrome1.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int main()
 {
-  char input[20],str[20];
+  char input[20],check[20];
   int i,j,len,count=0;
   scanf("%s",&input);
   len=strlen(input);
   j=len-1;
   for(i=0;i<len;i++)
   {
-   str[i]=input[j];
+   check[i]=input[j];
    j--;
   }
   for(i=0;i<len;i++)
   {
-    if(input[i] != str[i])
+    if(input[i] != check[i])
     {
       count = 1;
     }
