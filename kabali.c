7efb922530ecba54cbd40ef8920a5bd2d4d52b2d
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-25 21:45:07 +0530

    Update kabali.c

--- kabali.c
+++ kabali.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-  int kabalis_clan,opponents_clan,difference;
-  scanf("%d %d",&kabalis_clan,&opponents_clan);
-  difference=opponent's_clan-kabalis_clan;
+  int kabalis_clan,opnts_clan,difference;
+  scanf("%d %d",&kabalis_clan,&oppnts_clan);
+  difference=oppnts_clan-kabalis_clan;
   printf("%d",difference);
  
   return 0;
