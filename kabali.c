977717e8e194dc9f1134b133a07cde14233e6eb7
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-25 21:44:12 +0530

    Create kabali.c

---
+++ kabali.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int kabalis_clan,opponents_clan,difference;
+  scanf("%d %d",&kabalis_clan,&opponents_clan);
+  difference=opponent's_clan-kabalis_clan;
+  printf("%d",difference);
+ 
+  return 0;
+}
