a2452cb15d03de9efbe5aebfea0d12bd74f206af
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-25 21:10:17 +0530

    Rename palindrome.c to palindrome1.c

---
+++ palindrome1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+  char input[20],str[20];
+  int i,j,len,count=0;
+  scanf("%s",&input);
+  len=strlen(input);
+  j=len-1;
+  for(i=0;i<len;i++)
+  {
+   str[i]=input[j];
+   j--;
+  }
+  for(i=0;i<len;i++)
+  {
+    if(input[i] != str[i])
+    {
+      count = 1;
+    }
+  }
+  if(count == 0)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("No");
+  }
+  return 0;
+}
