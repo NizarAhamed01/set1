89b4d9be26878046e1aa9b45a5682a62bb169b75
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-04-30 09:27:23 +0530

    space

---
+++ space.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50];
+int i,count=0;
+clrscr();
+printf("\n Enter The String = ");
+gets(str);
+for(i=0;str[i];i++)
+if(str{i]=='')
+count++;
+printf("\n Total Spaces:%d",count);
+getch();
+}
