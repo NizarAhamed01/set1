18514d50966bf07aeb5ea1d8fd4022ee06533653
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-05-01 21:24:44 +0530

    Update kth_element.c

--- kth_element.c
+++ kth_element.c
@@ -3,14 +3,14 @@ int main()
 {
   int num;
   scanf("%d",&n);
-  int array[num],sum=0,i;
+  int input[num],sum=0,i;
   for(i=0;i<num;i++)
   {
-    scanf("%d",&array[i]);
+    scanf("%d",&input[i]);
   }
   for(i=0;i<num;i++)
   {
-    sum=sum+array[i];
+    sum=sum+input[i];
   }
   printf("%d",sum);
   return 0;
