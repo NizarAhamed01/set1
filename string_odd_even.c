f077cf95f435318e4a3b9b601e221bd29ecc2a20
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-05-01 21:15:39 +0530

    Update string_odd_even.c

--- string_odd_even.c
+++ string_odd_even.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-  char str1[20],str2[20]={0},str3[20]={0};
+  char input1[20],inpu2[20]={0},input3[20]={0};
   int i,j=0,k=0;
-  scanf("%s",str1);
-  for(i=0;str1[i] != '\0';i++)
+  scanf("%s",input1);
+  for(i=0;input1[i] != '\0';i++)
   {
     if(i % 2)
     {
-      str2[j] = str1[i];
+      input2[j] = input[i];
       j++;
     }
     else
     {
-      str3[k] = str1[i];
+      input3[k] = input1[i];
       k++;
     }
   }
-  printf("%s %s",str2,str3);
+  printf("%s %s",input2,input3);
   return 0;
 }
