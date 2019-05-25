2e87ea4f7b7df8bbe753a7abb8a4dea81d1f39c9
NizarAhamed01 <49307971+NizarAhamed01@users.noreply.github.com>
2019-05-25 12:36:11 +0530

    Update isogram.py

--- isogram.py
+++ isogram.py
@@ -1,13 +1,13 @@
-str=input()
+s=input()
 L=[]
-str1 = ''
+s1 = ''
 index = 0
-for index in str:
+for index in s:
   if index not in L:
     L.append(index)
-str1=''.join(L)
-Length1=len(str)
-Length2=len(str1)
+s1=''.join(L)
+Length1=len(s)
+Length2=len(s1)
 if(Length1 - Length2 == 0):
   print("yes")
 else:
